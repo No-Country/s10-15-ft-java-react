8b87f3a554b494b2c69d268fd126d7b1c4e8b8f0
Exequiel <exequielcostabel@gmail.com>
2023-08-23 11:06:19 -0300

    adding Navbar

--- Frontend/src/components/Users.jsx
+++ Frontend/src/components/Users.jsx
@@ -4,7 +4,7 @@ import { NavBar } from './NavBar';
 
 export const Users = () => {
   return (
-    <div className='text-lg box-border w-4/5 px-10 py-4'>
+    <div className='text-lg box-border w-4/5 px-10 py-5'>
         <NavBar />
         <FilterNav  />
     </div>
