98629df4f609267f9bfbab2dedf83e69e8e8a6af
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-08-23 16:41:06 -0300

    dynamic rendering, adding links file, userContext file and login component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -10,32 +10,54 @@ import { Users } from './components/Users'
 import { Settings } from './components/Settings'
 import { NewUser } from './components/NewUser'
 import { NavBar } from './components/NavBar'
+import Login from './components/Login'
+import { useEffect, useState } from 'react'
+import { UserContext } from './libs/context/useContext'
 
 function App() {
-
-
+  //logical part of user logged
+  const [log, setLog] = useState(false)
+  useEffect(() => {
+    setLog(localStorage.getItem('log') ?? '')
+  }, [])
+  useEffect(() => {
+    localStorage.setItem('log', log)
+  }, [log])
   return (
-    <>
     <BrowserRouter>
-    <div className='flex text-black'>
-      <SideBar />
-      <div className='flex flex-col w-full'>
-
-      <NavBar  />
-      <Routes>
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/inventario' element={<Inventory />} />
-        <Route path='/proveedores' element={<Suppliers />} />
-        <Route path='/historial' element={<History />} />
-        <Route path='/usuarios' element={<Users />} />
-        <Route path='/nuevoUsuario' element={<NewUser />} />
-        <Route path='/configuracion' element={<Settings />} />
-        <Route path='/ayuda' element={<Help />} />
-      </Routes>
-      </div>
-    </div>
+      <UserContext.Provider
+        value={{
+          log,
+          setLog
+        }}
+      >
+        <div className='flex text-black'>
+          {
+            //diferencial rendering if user are logged
+            log === true ? (
+              <>
+                <SideBar />
+                <div className='flex flex-col w-full'>
+                  <NavBar />
+                  <Routes>
+                    <Route path='/dashboard' element={<Dashboard />} />
+                    <Route path='/inventario' element={<Inventory />} />
+                    <Route path='/proveedores' element={<Suppliers />} />
+                    <Route path='/historial' element={<History />} />
+                    <Route path='/usuarios' element={<Users />} />
+                    <Route path='/nuevoUsuario' element={<NewUser />} />
+                    <Route path='/configuracion' element={<Settings />} />
+                    <Route path='/ayuda' element={<Help />} />
+                  </Routes>
+                </div>
+              </>
+            ) : (
+              <Login />
+            )
+          }
+        </div>
+      </UserContext.Provider>
     </BrowserRouter>
-    </>
   )
 }
 
