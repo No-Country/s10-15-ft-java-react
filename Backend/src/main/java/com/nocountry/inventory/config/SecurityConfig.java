aacee6df184384cd2bf7110379f4a3b35fc27e1b
duban4 <75090589+duban4@users.noreply.github.com>
2023-08-23 15:49:37 -0500

    r

--- Backend/src/main/java/com/nocountry/inventory/config/SecurityConfig.java
+++ Backend/src/main/java/com/nocountry/inventory/config/SecurityConfig.java
@@ -8,15 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -42,25 +49,25 @@ public class SecurityConfig {
                 .sessionManagement(session->{
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
-
-
+                .cors(Customizer.withDefaults())
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
 
-    @Bean
-    public WebMvcConfigurer corsFilter(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping(("/auth/login"))
-                        .allowedOrigins("http://localhost:8084")
-                        .allowedMethods("*");
-            }
-        };
-    }
+  @Bean
+    CorsConfigurationSource courseF(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:8084","localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+
+      UrlBasedCorsConfigurationSource sourse = new UrlBasedCorsConfigurationSource();
+      sourse.registerCorsConfiguration("/**",configuration);
+      return sourse;
+
+  }
 
 
 }
