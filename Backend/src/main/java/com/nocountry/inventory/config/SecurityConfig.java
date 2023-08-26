89a04de8bc98242061b59f7fa63a090ed6f6135a
duban4 <75090589+duban4@users.noreply.github.com>
2023-08-25 22:38:24 -0500

    p-cors

--- Backend/src/main/java/com/nocountry/inventory/config/SecurityConfig.java
+++ Backend/src/main/java/com/nocountry/inventory/config/SecurityConfig.java
@@ -60,8 +60,8 @@ public class SecurityConfig {
   @Bean
     CorsConfigurationSource courseF(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8084","https://s10-15-ft-java-react-production.up.railway.app/"));
-        configuration.setAllowedMethods(List.of("GET"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8084","https://stockflows10.vercel.app/"));
+        configuration.setAllowedMethods(List.of("GET","POST"));
         configuration.setAllowedHeaders(List.of("Authorization"));
 
       UrlBasedCorsConfigurationSource sourse = new UrlBasedCorsConfigurationSource();
