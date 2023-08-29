e2b1d6ed58495d1eb88cef66a67ed5e38bc47b38
Edgar Valentin Ruiz Padilla <edgarvrp22@gmail.com>
2023-08-28 21:23:08 -0600

    Se añade Localhost de front en security, se carga el script SQL y el modelo ER

--- Backend/SQL/Query.sql
+++ Backend/SQL/Query.sql
@@ -1,14 +1,27 @@
+
 -- Creación de la tabla Usuarios
-CREATE TABLE s10Users (
+CREATE TABLE s_Users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     rol ENUM('ADMINISTRADOR', 'VENDEDOR', 'ALMACEN') NOT NULL,
-    otherDetails TEXT
+    otherDetails TEXT,
+    currencyType VARCHAR(50),
+    showDecimals BOOLEAN,
+    darkMode BOOLEAN,
+    email VARCHAR(255)
+);
+
+-- Creación de la tabla Proveedores
+CREATE TABLE s_Providers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    providerName VARCHAR(255) NOT NULL,
+    category VARCHAR(255),
+    contactPhoneNumber VARCHAR(20)
 );
 
 -- Creación de la tabla Productos
-CREATE TABLE s10Products (
+CREATE TABLE s_Products (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     productName VARCHAR(255) NOT NULL,
     itemCode VARCHAR(50) NOT NULL,
@@ -23,7 +36,7 @@ CREATE TABLE s10Products (
 );
 
 -- Creación de la tabla Transacciones
-CREATE TABLE s10transactions (
+CREATE TABLE s_Transactions (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     IDProduct INT,
     transactionType ENUM('entrada', 'salida') NOT NULL,
@@ -35,7 +48,7 @@ CREATE TABLE s10transactions (
 );
 
 -- Creación de la tabla Ajustes de Inventario
-CREATE TABLE s10SettingsInventory (
+CREATE TABLE s_SettingsInventory (
     ID INT PRIMARY KEY,
     IDProduct INT,
     adjustedQuantity INT NOT NULL,
@@ -47,7 +60,7 @@ CREATE TABLE s10SettingsInventory (
 );
 
 -- Creación de la tabla Notificaciones
-CREATE TABLE s10Notifications (
+CREATE TABLE s_Notifications (
     ID INT PRIMARY KEY,
     IDUser INT,
     messageNotification TEXT NOT NULL,
@@ -56,21 +69,13 @@ CREATE TABLE s10Notifications (
 );
 
 -- Creación de la tabla Categorías de Productos
-CREATE TABLE s10CategoriesProducts (
+CREATE TABLE s_CategoriesProducts (
     ID INT PRIMARY KEY,
     nameCategory VARCHAR(255) NOT NULL
 );
 
--- Creación de la tabla Permisos
-CREATE TABLE s10Permissions (
-    ID INT PRIMARY KEY,
-    IDUser INT,
-    permissionSpecific BOOLEAN,
-    INDEX IDUser_idx (IDUser)
-);
-
 -- Creación de la tabla Historial de Búsquedas
-CREATE TABLE s10historySearches (
+CREATE TABLE s_HistorySearches (
     ID INT PRIMARY KEY,
     IDUser INT,
     searchTerm VARCHAR(255) NOT NULL,
@@ -80,6 +85,7 @@ CREATE TABLE s10historySearches (
 
 
 
+-------------------------------------------------------------------------------------------------------------------------------
 
 
 
@@ -88,19 +94,43 @@ CREATE TABLE s10historySearches (
 
 
 
-----------------------------------------------------------------
+
+
+
+
+
+
+
 -- Creación de la tabla Usuarios
-CREATE TABLE sUsers (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE s_Users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     rol ENUM('ADMINISTRADOR', 'VENDEDOR', 'ALMACEN') NOT NULL,
-    otherDetails TEXT
+    otherDetails TEXT,
+    currencyType VARCHAR(50),
+    showDecimals BOOLEAN,
+    darkMode BOOLEAN,
+    email VARCHAR(255)
+);
+
+-- Creación de la tabla Proveedores
+CREATE TABLE s_Providers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    providerName VARCHAR(255) NOT NULL,
+    category VARCHAR(255),
+    contactPhoneNumber VARCHAR(20)
+);
+
+-- Creación de la tabla Categorías de Productos
+CREATE TABLE s_CategoriesProducts (
+    ID INT PRIMARY KEY,
+    nameCategory VARCHAR(255) NOT NULL
 );
 
 -- Creación de la tabla Productos
-CREATE TABLE sProducts (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE s_Products (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     productName VARCHAR(255) NOT NULL,
     itemCode VARCHAR(50) NOT NULL,
     category INT,
@@ -108,63 +138,51 @@ CREATE TABLE sProducts (
     quantityStock INT NOT NULL,
     pathImage VARCHAR(255),
     locationDeposit VARCHAR(255),
-    provider VARCHAR(255),
+    provider INT,
     description TEXT,
-    warehouseStatus VARCHAR(50)
+    warehouseStatus VARCHAR(50),
+    FOREIGN KEY (category) REFERENCES s_CategoriesProducts(ID),
+    FOREIGN KEY (provider) REFERENCES s_Providers(id)
 );
 
 -- Creación de la tabla Transacciones
-CREATE TABLE s10transactions (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE s_Transactions (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     IDProduct INT,
     transactionType ENUM('entrada', 'salida') NOT NULL,
     quantity INT NOT NULL,
     dateTimeTransaction DATETIME NOT NULL,
     IDUser INT,
-    FOREIGN KEY (IDProduct) REFERENCES sProducts(ID),
-    FOREIGN KEY (IDUser) REFERENCES sUsers(ID)
+    FOREIGN KEY (IDProduct) REFERENCES s_Products(id),
+    FOREIGN KEY (IDUser) REFERENCES s_Users(id)
 );
 
 -- Creación de la tabla Ajustes de Inventario
-CREATE TABLE s10SettingsInventory (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE s_SettingsInventory (
+    ID INT PRIMARY KEY,
     IDProduct INT,
     adjustedQuantity INT NOT NULL,
     reasonAdjustment TEXT NOT NULL,
     dateTimeSetting DATETIME NOT NULL,
     IDUser INT,
-    FOREIGN KEY (IDProduct) REFERENCES s10Products(ID),
-    FOREIGN KEY (IDUser) REFERENCES s10Users(ID)
+    FOREIGN KEY (IDProduct) REFERENCES s_Products(id),
+    FOREIGN KEY (IDUser) REFERENCES s_Users(id)
 );
 
 -- Creación de la tabla Notificaciones
-CREATE TABLE s10Notifications (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE s_Notifications (
+    ID INT PRIMARY KEY,
     IDUser INT,
     messageNotification TEXT NOT NULL,
     dateTimeNotification DATETIME NOT NULL,
-    FOREIGN KEY (IDUser) REFERENCES s10Users(ID)
-);
-
--- Creación de la tabla Categorías de Productos
-CREATE TABLE s10CategoriesProducts (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    nameCategory VARCHAR(255) NOT NULL
-);
-
--- Creación de la tabla Permisos
-CREATE TABLE s10Permissions (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    IDUser INT,
-    permissionSpecific BOOLEAN, -- Pueden ser varios campos booleanos según los permisos necesarios
-    FOREIGN KEY (IDUser) REFERENCES s10Users(ID)
+    FOREIGN KEY (IDUser) REFERENCES s_Users(id)
 );
 
 -- Creación de la tabla Historial de Búsquedas
-CREATE TABLE s10historySearches (
-    ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+CREATE TABLE s_HistorySearches (
+    ID INT PRIMARY KEY,
     IDUser INT,
     searchTerm VARCHAR(255) NOT NULL,
     dateTimeSearch DATETIME NOT NULL,
-    FOREIGN KEY (IDUser) REFERENCES s10Users(ID)
+    FOREIGN KEY (IDUser) REFERENCES s_Users(id)
 );
