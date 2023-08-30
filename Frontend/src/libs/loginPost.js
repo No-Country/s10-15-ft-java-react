b366942e5ab0d2a7cd4497dc955bf9abe1300a9c
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-08-30 14:11:08 -0300

    deleting console.log`s

--- Frontend/src/libs/loginPost.js
+++ Frontend/src/libs/loginPost.js
@@ -3,7 +3,6 @@ import axios from "axios"
 
 //Logical component for a login request
 const LoginPost = async({user, pass, setStat, API_URL})=>{
-console.log(user)
      await axios
     .post(
       `${API_URL || 'https://stockflow.up.railway.app'}/auth/login`,
