0c83e7aaf31286d8a69bc1c0f3aab480c191df0a
EdgarVRP <35878706+EdgarVRP@users.noreply.github.com>
2023-09-01 12:50:03 -0600

    Se añaden imports de spring y CORS

--- Backend/src/main/java/com/nocountry/inventory/controller/ProductController.java
+++ Backend/src/main/java/com/nocountry/inventory/controller/ProductController.java
@@ -4,6 +4,7 @@ import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- Backend/src/main/java/com/nocountry/inventory/controller/UserController.java
+++ Backend/src/main/java/com/nocountry/inventory/controller/UserController.java
@@ -6,6 +6,7 @@ import com.nocountry.inventory.service.UserEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
