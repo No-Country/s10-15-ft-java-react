ed0d71836262b1687b9488ae1d8150c809240c03
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-09-07 18:22:33 -0300

    commit before the pull

--- Frontend/src/libs/productPost.js
+++ Frontend/src/libs/productPost.js
@@ -1,40 +1,5 @@
 import axios from 'axios';
 //const API_URL = import.meta.env.VITE_API_PRODUCT_URL;
-<<<<<<< HEAD
 const createProduct = (productData) => {
 axios.post(`https://s10-15-ft-java-react-production.up.railway.app/product/save`, productData,)};
 export default createProduct;
-=======
-
-
-
-const createProduct = async (productData) => {
-    try {
-            const resp = await axios.post(`https://s10-15-ft-java-react-production.up.railway.app/product/save`, productData, 
-              {
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-              }
-                
-            );
-
-            if (resp) {
-              resp
-                
-            } else {
-              resp
-            }
-        } catch (error) {
-            error
-        }
-    };
-
-
-
-    
-
-    
-
-    export default createProduct;
->>>>>>> 80c1fd76eac70281523d0acb48bf903c2b6f8e76
