fe1298c2f2e559b562f4fb575b39ee51ce52b31a
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-09-13 12:06:03 -0300

    pushing

--- Frontend/src/libs/usersRequest/userPost.js
+++ Frontend/src/libs/usersRequest/userPost.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-//const API_URL = import.meta.env.VITE_API_PRODUCT_URL;
 const userPost = async (userData) => {
   await axios.post(
     `https://s10-15-ft-java-react-production.up.railway.app/user`,
