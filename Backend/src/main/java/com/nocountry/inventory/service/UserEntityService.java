bf840df92a12d8cf576f4778f232c4bcbf962988
fernando agustin perez <98239565+bobony97@users.noreply.github.com>
2023-09-12 22:52:37 -0300

    Update UserEntityService.java
    Signed-off-by: fernando agustin perez <98239565+bobony97@users.noreply.github.com>

--- Backend/src/main/java/com/nocountry/inventory/service/UserEntityService.java
+++ Backend/src/main/java/com/nocountry/inventory/service/UserEntityService.java
@@ -6,6 +6,7 @@ import com.nocountry.inventory.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
